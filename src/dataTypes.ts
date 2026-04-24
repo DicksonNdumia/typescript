b068084fdde978e5c4604eb3dace53777362deb1
DicksonNdumia <ndumiadickson19@gmail.com>
2026-04-25 00:02:22 +0300

    first commit

---
+++ src/dataTypes.ts
@@ -0,0 +1,27 @@
+let name = "John Doe"
+let age = 3;
+console.log(age);
+console.log(name)
+
+let newName = 'Mark Zuck'
+const  updatedName = newName.toUpperCase();
+console.log(updatedName);
+
+
+let isOnline = true;
+
+console.log(isOnline);
+
+let dob = "25"
+
+let result = parseInt(dob);
+console.log(result);
+
+
+let isValid: boolean = false;
+
+console.log(isValid);
+
+// age = "John" //  Type string is not assignable to type number
+
+
